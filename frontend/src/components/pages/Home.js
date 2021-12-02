cdb4bf2d8d1dc1a7e7ac776a3ebec58546eec503
Kevin Barrera <kbarrerac@unal.edu.co>
2021-12-02 18:58:23 -0500

    NewHeaders and schemas

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,13 +1,13 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import '../css/main.css'
-import '../css/formatoHome.css'
-import '../plugins/fontawesome-free/css/all.min.css'
-import'../dist/css/adminlte.min.css'
-import Header from '../Header/Header'
+import "../css/main.css";
+import "../css/formatoHome.css";
+import "../plugins/fontawesome-free/css/all.min.css";
+import "../dist/css/adminlte.min.css";
+import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
-const Home = ({history}) => {
+const Home = ({ history }) => {
   const [error, setError] = useState("");
   const [homeData, setHomeData] = useState("");
 
@@ -23,7 +23,7 @@ const Home = ({history}) => {
       try {
         const { data } = await axios.get("/api/home", config);
         setHomeData(data.data);
-        console.log(homeData)
+        console.log(homeData);
       } catch (error) {
         localStorage.removeItem("authToken");
         setError("You are not authorized please login");
@@ -33,69 +33,128 @@ const Home = ({history}) => {
     fetchHomeDate();
   }, [history]);
 
-  
   return error ? (
     <span className="error-message">{error}</span>
   ) : (
     <div>
-      <Header/>
-    <div className="home">
-  
-
-    <div class="container">
-    <h1 class='tituloHome'>Equipo 4</h1>
-
-<h2 >Proyecto Ventas</h2>
-
-<h3> Integrantes del grupo</h3>
-
-<br/>
-
-    <table class="integrantes" >
-        
-          <tr>
-            
-            <th scope="col">Nombre</th>
-            <th scope="col">Email</th>
-            
-          </tr>
-        
-        <tbody>
-          <tr>
-           
-            <td>Andrés Coronado</td>
-            <td>andrescoronadob5@gmail.com</td>
-           
-          </tr>
-          <tr>
-           
-            <td>Julián Andrés Ortiz</td>
-            <td>julianortizvillar@gmail.com</td>
-            
-          </tr>
-          <tr>
-           
-            <td>Juan Carlos Valderrama</td>
-            <td> juancavalde@gmail.com</td>
-            
-          </tr>
-
-          <tr>
-           
-            <td>Sebastian Barrera</td>
-            <td> kbarrerac@unal.edu.co</td>
-            
-          </tr>
-
-        </tbody>
-      
-      </table>
-     
+      <Header />
+      <div>
+        <body className="bg-light">
+          <div className="container-fluid flex-column p-0 h-100">
+            <Header
+            //isLoggedIn={isLoggedIn}
+            //setLogin={setLogin}
+            //setValidate={setValidate}
+            />
+            <div className="proyectTitle d-flex justify-content-center">
+              <h1 className="text-center p-3 pt-4 text-dark">
+                SOBRE LOS AUTORES...
+              </h1>
+            </div>
+            <div className="text-center px-5">
+              El proyecto de "Titulo del proyecto" hace parte del desarrollo del
+              curso dictado por el Ministerio de Tecnologías de la Información y
+              la Comunicación (MINTIC). Los autores que han desarrolado este
+              sistema de gestión de proyectos son:
+            </div>
+            <div className="proyect-authors flex-column">
+              <div className="up-authors row mt-4 mb-5 w-100 ">
+                <div className="col-lg-2 mx-auto">
+                  <div className="text-center">
+                    <svg
+                      className="bd-placeholder-img rounded-circle m-1"
+                      width="140"
+                      height="140"
+                      xmlns="http://www.w3.org/2000/svg"
+                      role="img"
+                      aria-label="Placeholder: 140x140"
+                      preserveAspectRatio="xMidYMid slice"
+                      focusable="false"
+                    >
+                      <title>Placeholder</title>
+                      <rect width="100%" height="100%" fill="#777"></rect>
+                      <text x="50%" y="50%" fill="#777" dy=".3em">
+                        140x140
+                      </text>
+                    </svg>
+                    <h3 className="text-center">Andres Coronado</h3>
+                    <span>e-mail: coronadoandresfelipe@gmail.com</span>
+                  </div>
+                </div>
+                <div className="col-lg-2 mx-auto">
+                  <div className="text-center">
+                    <svg
+                      className="bd-placeholder-img rounded-circle m-1"
+                      width="140"
+                      height="140"
+                      xmlns="http://www.w3.org/2000/svg"
+                      role="img"
+                      aria-label="Placeholder: 140x140"
+                      preserveAspectRatio="xMidYMid slice"
+                      focusable="false"
+                    >
+                      <title>Placeholder</title>
+                      <rect width="100%" height="100%" fill="#777"></rect>
+                      <text x="50%" y="50%" fill="#777" dy=".3em">
+                        140x140
+                      </text>
+                    </svg>
+                    <h3 className="text-center">Julian Ortiz</h3>
+                    <span>e-mail: Julianortizvillar@hotmail.com</span>
+                  </div>
+                </div>
+                <div className="col-lg-2 mx-auto">
+                  <div className="text-center">
+                    <svg
+                      className="bd-placeholder-img rounded-circle m-1"
+                      width="140"
+                      height="140"
+                      xmlns="http://www.w3.org/2000/svg"
+                      role="img"
+                      aria-label="Placeholder: 140x140"
+                      preserveAspectRatio="xMidYMid slice"
+                      focusable="false"
+                    >
+                      <title>Placeholder</title>
+                      <rect width="100%" height="100%" fill="#777"></rect>
+                      <text x="50%" y="50%" fill="#777" dy=".3em">
+                        140x140
+                      </text>
+                    </svg>
+                    <h3 className="text-center">Juan carlos Valderrama</h3>
+                    <span>e-mail: juancavalde@gmail.com</span>
+                  </div>
+                </div>
+                <div className="col-lg-2 mx-auto">
+                  <div className="text-center">
+                    <svg
+                      className="bd-placeholder-img rounded-circle m-1"
+                      width="140"
+                      height="140"
+                      xmlns="http://www.w3.org/2000/svg"
+                      role="img"
+                      aria-label="Placeholder: 140x140"
+                      preserveAspectRatio="xMidYMid slice"
+                      focusable="false"
+                    >
+                      <title>Placeholder</title>
+                      <rect width="100%" height="100%" fill="#777"></rect>
+                      <text x="50%" y="50%" fill="#777" dy=".3em">
+                        140x140
+                      </text>
+                    </svg>
+                    <h3 className="text-center">Kevin Sebastian Barrera</h3>
+                    <span>e-mail: kbarrerac@unal.edu.co</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </body>
+      </div>
+      <Footer></Footer>
     </div>
-    <Footer></Footer>
-  </div>
-  
-  </div>
+    //</div>
   );
 };
 
--- frontend/src/components/pages/User.js
+++ frontend/src/components/pages/User.js
@@ -1,15 +1,14 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import '../css/main.css'
-import '../css/Productos.css'
-import '../plugins/fontawesome-free/css/all.min.css'
-import'../dist/css/adminlte.min.css'
-import '../css/crearProducto.css'
-import Header from '../Header/Header'
+import "../css/main.css";
+import "../css/Productos.css";
+import "../plugins/fontawesome-free/css/all.min.css";
+import "../dist/css/adminlte.min.css";
+import "../css/crearProducto.css";
+import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
-
-const User = ({history}) => {
+const User = ({ history }) => {
   const [error, setError] = useState("");
   const [username, setUserName] = useState("");
   const [email, setEmail] = useState("");
@@ -17,49 +16,39 @@ const User = ({history}) => {
   const [bDate, setBDate] = useState("");
   const [roles, setRoles] = useState("");
   const [identification, setIdentification] = useState("");
-const[actID,setActID]=useState("");
-const[actUserName,setActUserName]=useState("");
-const [actEmail, setActEmail] = useState("");
-const [actPhone, setActPhone] = useState("");
-const [actBDate, setActBDate] = useState("");
-const [actRoles, setActRoles] = useState("");
-const [actIdentification, setActIdentification] = useState("");
-  const [userData, setUserData] = useState([
-    
-  ]);
- 
-//funciones globales
-//headerde conexion
+  const [actID, setActID] = useState("");
+  const [actUserName, setActUserName] = useState("");
+  const [actEmail, setActEmail] = useState("");
+  const [actPhone, setActPhone] = useState("");
+  const [actBDate, setActBDate] = useState("");
+  const [actRoles, setActRoles] = useState("");
+  const [actIdentification, setActIdentification] = useState("");
+  const [userData, setUserData] = useState([]);
+
+  //funciones globales
+  //headerde conexion
   const config = {
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${localStorage.getItem("authToken")}`,
     },
   };
-//actualizar pagina
-  const updatePage = async ()=>{
-      try{
-    const { data } = await axios.get("/api/auth", config);
-    setUserData(data);
-      }
-      catch{
-console.log(error);
-      }
-    };
+  //actualizar pagina
+  const updatePage = async () => {
+    try {
+      const { data } = await axios.get("/api/auth", config);
+      setUserData(data);
+    } catch {
+      console.log(error);
+    }
+  };
 
-  
-   
-
-
-
-//cargar tabla
+  //cargar tabla
   useEffect(() => {
     const fetchUserDate = async () => {
-     
       try {
-       
-        updatePage()
-      } catch  {
+        updatePage();
+      } catch {
         localStorage.removeItem("authToken");
         setError("You are not authorized please login");
       }
@@ -67,238 +56,256 @@ console.log(error);
 
     fetchUserDate();
   }, [history]);
-///Borrar usuario
+  ///Borrar usuario
   const deleteUser = async (userId) => {
-      try{
-    const response = window.confirm('are you sure you want to delete this user?');
-    if (response) {    
-
-         axios.delete('/api/auth/' + userId,config);
-        
-        
-    }
-    updatePage();
-}
-
-    catch {
-        localStorage.removeItem("authToken");
-        setError("You are not authorized please login");
+    try {
+      const response = window.confirm(
+        "are you sure you want to delete this user?"
+      );
+      if (response) {
+        axios.delete("/api/auth/" + userId, config);
       }
-
-    
-}
-//editar usuario
-const actualizarUser = async (userId) => {
-
-    try{
-  const response = window.confirm('are you sure you want to update this user?');
-  if (response) {
-   var userNameAct=document.getElementById("userName2").value
-   var emailAct=document.getElementById("email2").value
-   var phoneAct=document.getElementById("phone2").value
-   var bDateAct=document.getElementById("bDate2").value
-   var rolesAct=document.getElementById("roles2").value
-   var identificationAct=document.getElementById("identification2").value
-        const data= await axios.put('/api/auth/' + userId,
-              { username:userNameAct,
-                email:emailAct,
-                phone:phoneAct,
-                bDate:bDateAct,
-                roles:rolesAct,
-                identification:identificationAct
-               }
-      ,      
-      config);
-      console.log(data)
-      cerrarModalEditar();
-    updatePage();
-  }
-}
-catch {
-    localStorage.removeItem("authToken");
-    setError("You are not authorized please login");
-}
-};
-
-
-
-
-
-
-var modal2 = document.getElementById("editar");
-
-
-const mostrarModalEditar = () => {
-  modal2.style.display = "block";
-};
-
-const cerrarModalEditar = () => {
-  modal2.style.display = "none";
-};
-
-
-
-const tomarDato= async(idPro,proUserName,proEmail,proPhone,probDate,proRoles,proIdentification)=>{
-    try{
-          setActID(idPro);
-  setActUserName(proUserName);
-setActEmail(proEmail)
- setActPhone(proPhone);
-setActBDate(probDate);
-  setActRoles(proRoles);
-  setActIdentification(proIdentification)
-
-console.log(idPro)
-console.log(proUserName+proEmail+proPhone+probDate+proRoles+proIdentification)
-
-document.getElementById("userName2").value=proUserName;
-document.getElementById("email2").value=proEmail;
-document.getElementById("phone2").value=proPhone;
-document.getElementById("bDate2").value=probDate;
-document.getElementById("roles2").value=proRoles;
-document.getElementById("identification2").value=proIdentification;
-mostrarModalEditar();
+      updatePage();
+    } catch {
+      localStorage.removeItem("authToken");
+      setError("You are not authorized please login");
     }
-    catch{
-        console.log(error);
-              }
+  };
+  //editar usuario
+  const actualizarUser = async (userId) => {
+    try {
+      const response = window.confirm(
+        "are you sure you want to update this user?"
+      );
+      if (response) {
+        var userNameAct = document.getElementById("userName2").value;
+        var emailAct = document.getElementById("email2").value;
+        var phoneAct = document.getElementById("phone2").value;
+        var bDateAct = document.getElementById("bDate2").value;
+        var rolesAct = document.getElementById("roles2").value;
+        var identificationAct =
+          document.getElementById("identification2").value;
+        const data = await axios.put(
+          "/api/auth/" + userId,
+          {
+            username: userNameAct,
+            email: emailAct,
+            phone: phoneAct,
+            bDate: bDateAct,
+            roles: rolesAct,
+            identification: identificationAct,
+          },
+          config
+        );
+        console.log(data);
+        cerrarModalEditar();
+        updatePage();
+      }
+    } catch {
+      localStorage.removeItem("authToken");
+      setError("You are not authorized please login");
+    }
+  };
 
-};
-  
+  var modal2 = document.getElementById("editar");
+
+  const mostrarModalEditar = () => {
+    modal2.style.display = "block";
+  };
+
+  const cerrarModalEditar = () => {
+    modal2.style.display = "none";
+  };
+
+  const tomarDato = async (
+    idPro,
+    proUserName,
+    proEmail,
+    proPhone,
+    probDate,
+    proRoles,
+    proIdentification
+  ) => {
+    try {
+      setActID(idPro);
+      setActUserName(proUserName);
+      setActEmail(proEmail);
+      setActPhone(proPhone);
+      setActBDate(probDate);
+      setActRoles(proRoles);
+      setActIdentification(proIdentification);
+
+      console.log(idPro);
+      console.log(
+        proUserName +
+          proEmail +
+          proPhone +
+          probDate +
+          proRoles +
+          proIdentification
+      );
+
+      document.getElementById("userName2").value = proUserName;
+      document.getElementById("email2").value = proEmail;
+      document.getElementById("phone2").value = proPhone;
+      document.getElementById("bDate2").value = probDate;
+      document.getElementById("roles2").value = proRoles;
+      document.getElementById("identification2").value = proIdentification;
+      mostrarModalEditar();
+    } catch {
+      console.log(error);
+    }
+  };
+
   return error ? (
     <span className="error-message">{error}</span>
   ) : (
-       <div>
-           <Header/>
-           <div id="titlepro">
-           <h1>Lista Usuarios</h1>
-           </div>
-              <div className="productos">
-             <table  >
-              <tr>
-                <th>Username</th>
-                <th>Email</th>
-                <th>Phone</th>
-                <th>Date</th>
-                <th>Rol</th>
-                <th>Cedula</th>
-                <th></th>
-                <th></th>
-                       </tr>
-                      <tbody>
-              {userData.map((pro ) => (
-                <tr  value={pro._id} key={pro._id}>
-                  <td>{pro.username}</td>
-                  <td>{pro.email}</td>
-                  <td>{pro.phone}</td>
-                  <td>{pro.bDate}</td>
-                  <td>{pro.roles}</td>
-                  <td>{pro.identification}</td>
-                <td><button  className="btn btn-success" onClick={()=>tomarDato(pro._id,pro.username,pro.email,pro.phone,pro.bDate,pro.roles,pro.identification)} >Actualizar</button>  </td>
-                <td><button  className="btn btn-danger" onClick={() => deleteUser(pro._id)} >Delete</button>  </td>
+    <div>
+      <Header />
+      <div id="titlepro">
+        <h1>Lista Usuarios</h1>
+      </div>
+      <div className="productos">
+        <table>
+          <tr>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Date</th>
+            <th>Rol</th>
+            <th>Cedula</th>
+            <th></th>
+            <th></th>
+          </tr>
+          <tbody>
+            {userData.map((pro) => (
+              <tr value={pro._id} key={pro._id}>
+                <td>{pro.username}</td>
+                <td>{pro.email}</td>
+                <td>{pro.phone}</td>
+                <td>{pro.bDate}</td>
+                <td>{pro.roles}</td>
+                <td>{pro.identification}</td>
+                <td>
+                  <button
+                    className="btn btn-success"
+                    onClick={() =>
+                      tomarDato(
+                        pro._id,
+                        pro.username,
+                        pro.email,
+                        pro.phone,
+                        pro.bDate,
+                        pro.roles,
+                        pro.identification
+                      )
+                    }
+                  >
+                    Actualizar
+                  </button>{" "}
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => deleteUser(pro._id)}
+                  >
+                    Delete
+                  </button>{" "}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
 
-               </tr>
-              ))}
-            </tbody>
-          </table>
+        <div id="editar" class="modal">
+          <div class="modal-content">
+            <div className="form-group">
+              <div>
+                <h1>Actualizar User</h1>
+              </div>
+              <form>
+                <label htmlFor="username">Username:</label>
+                <br />
+                <div>
+                  <input
+                    type="text"
+                    id="userName2"
+                    onBlur={(e) => setUserName(e.target.value)}
+                  />
+                </div>
+                <label htmlFor="email">correo:</label>
+                <br />
+                <div>
+                  <input
+                    type="email"
+                    id="email2"
+                    onBlur={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <label htmlFor="phone">Celular:</label>
+                <br />
+                <div>
+                  <input
+                    type="number"
+                    id="phone2"
+                    onBlur={(e) => setPhone(e.target.value)}
+                  />
+                </div>
+                <label htmlFor="bDate">Birthday:</label>
+                <br />
+                <div>
+                  <input
+                    type="date"
+                    min="1900-01-01"
+                    max="2050-12-31"
+                    id="bDate2"
+                    onBlur={(e) => setBDate(e.target.value)}
+                  />
+                </div>
+                <label htmlFor="roles">Rol:</label>
+                <br />
+                <div>
+                  <input
+                    type="text"
+                    id="roles2"
+                    onBlur={(e) => setRoles(e.target.value)}
+                  />
+                </div>
 
-          
+                <label htmlFor="identification">Cedula:</label>
+                <br />
+                <div>
+                  <input
+                    type="number"
+                    id="identification2"
+                    onBlur={(e) => setIdentification(e.target.value)}
+                  />
+                </div>
+              </form>
+              <br />
+            </div>
+            <div>
+              <button
+                className="btn btn-success"
+                onClick={() => actualizarUser(actID)}
+              >
+                {" "}
+                Actualizar
+              </button>
+            </div>
 
-
-<div id="editar" class="modal" >
- <div class="modal-content">
-  <div className="form-group">
-  <div><h1>Actualizar User</h1></div>
-  <form>    
-          <label htmlFor="username">Username:</label>
-          <br/>
-          <div>
-          <input
-            type="text"
-              id="userName2"
-                 onBlur={(e) => setUserName(e.target.value)}
-                />
+            <br></br>
+            <div>
+              <button className="btn btn-danger" onClick={cerrarModalEditar}>
+                cancelar
+              </button>
+            </div>
           </div>
-          <label htmlFor="email">correo:</label>
-          <br/>
-          <div>
-          <input
-            type="email"
-               id="email2"
-                   onBlur={(e) => setEmail(e.target.value)}
-             />
-          </div>
-          <label htmlFor="phone">Celular:</label>
-          <br/>
-          <div>
-          <input
-            type="number"
-               id="phone2"
-                  onBlur={(e) => setPhone(e.target.value)}
-          />
-          </div>
-          <label htmlFor="bDate">Birthday:</label>
-          <br/>
-          <div>
-          <input
-           type="date"  
-
-           min="1900-01-01" max="2050-12-31"
-               id="bDate2"
-                  onBlur={(e) => setBDate(e.target.value)}
-           />
-          </div>
-          <label htmlFor="roles">Rol:</label>
-          <br/>
-          <div>
-          <input
-            type="text"
-               id="roles2"
-                   onBlur={(e) => setRoles(e.target.value)}
-               />
-          </div>
-
-          <label htmlFor="identification">Cedula:</label>
-          <br/>
-          <div>
-          <input
-            type="number"
-               id="identification2"
-                  onBlur={(e) => setIdentification(e.target.value)}
-          />
-          </div>
-
-          </form>
-          <br/>
         </div>
-        <div>
-        <button  className="btn btn-success" onClick={() => actualizarUser(actID)}  > Actualizar</button>
-        </div>
-       
-        <br></br>
-        <div>
-  <button className="btn btn-danger"  onClick={cerrarModalEditar}>cancelar</button>
-  </div>
-       
-  </div>
-</div>
-
-          </div>
-          <Footer></Footer>  
-  </div>
+      </div>
+      <Footer></Footer>
+    </div>
   );
-
-  
 };
-
-
-
-
-
-
-
-
-
-
 
 export default User;
